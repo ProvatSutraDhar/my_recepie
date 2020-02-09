d5d6f272febae6dc7bc4191ad58e90dc84140e44
Provat Sutradhar <s.provatcsd01@gmail.com>
2020-02-09 18:25:55 +0600

    add comment model and assosiation with recipe and chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,8 +5,9 @@ before_save { self.email = email.downcase }
   validates :email, presence: true, length:{ maximum: 255},
                           format:{with: VALID_EMAIL_REGEX },
                           uniqueness: {case_sensitive: false}
-    has_many :recipes
+    has_many :recipes, dependent: :destroy
     has_secure_password
-    validates :password, presence: true, length: {minimum: 5}
+    validates :password, presence: true, length: {minimum: 5}, allow_nil: true
+    has_many :commets, dependent: :destroy
 
 end
